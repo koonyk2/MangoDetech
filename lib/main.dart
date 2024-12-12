cfbedf38b15575524ac2138cb6c4977c078fe19a
Kajpol Putsaruksa <koonyk2@gmail.com>
2024-12-12 16:40:45 +0700

    FirstVersionMango

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+List<CameraDescription>? cameras;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(const CameraApp());
+}
+
+class CameraApp extends StatelessWidget {
+  const CameraApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CameraScreen(),
+    );
+  }
+}
+
+class CameraScreen extends StatefulWidget {
+  const CameraScreen({super.key});
+
+  @override
+  _CameraScreenState createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  late CameraController _controller;
+  bool _isCameraInitialized = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeCamera();
+  }
+
+  Future<void> _initializeCamera() async {
+    _controller = CameraController(
+      cameras![0], // กล้องตัวแรก (กล้องหลัง)
+      ResolutionPreset.high, // ความละเอียดสูง
+    );
+
+    await _controller.initialize();
+    if (!mounted) return;
+
+    setState(() {
+      _isCameraInitialized = true;
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Camera App")),
+      body: _isCameraInitialized
+          ? CameraPreview(_controller)
+          : const Center(child: CircularProgressIndicator()),
+    );
+  }
+}
